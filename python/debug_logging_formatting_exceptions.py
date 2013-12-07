4b84ddef89c23dc71947f6832d899782def16de2
Kevin Roy <kiniou@gmail.com>
2013-12-07 16:26:48 +0100

    add some extra debug informations

--- python/debug_logging_formatting_exceptions.py
+++ python/debug_logging_formatting_exceptions.py
@@ -4,16 +4,15 @@
 # (seen at http://stackoverflow.com/questions/2477934/easy-way-to-find-not-enough-arguments-in-python-logging-library)
 
 from pprint import pprint, pformat
-import sys
+import sys, traceback
 def print_log_record_on_error(func):
     def wrap(self, record):
         try:
-            print >>sys.stderr,pformat(record.__dict__)
-            print >>sys.stderr,pformat(self._fmt)
             return func(self, record)
         except:
-            print >>sys.stderr,"Unable to create log message fmt=%r, meg=%r " % (
-                                getattr(self, '_fmt', '?'), record.getMessage())
+            print >>sys.stderr,"Unable to create log message fmt=%r, msg=%r, msgargs=%r " % (
+                                getattr(self, '_fmt', '?'), record.msg, record.args)
+            print >>sys.stderr,''.join(traceback.format_stack())
             raise
     return wrap
 logging.Formatter.format = print_log_record_on_error(logging.Formatter.format)
